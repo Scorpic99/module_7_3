9e98b457d19f29eb3a9ab6e002ec3a87228d7c0f
Александр <116272931+Scorpic99@users.noreply.github.com>
2024-10-27 14:53:06 +0300

    Update module_7_3_update.py

--- module_7_3.py
+++ module_7_3.py
@@ -1,19 +1,18 @@
-
+import re
 class WordsFinder:
-    def __init__(self, *args):
-        self.args = list(args)
+    def __init__(self, *file_names):
+        self.file_names = list(file_names)
 
     def get_all_words(self):
         all_words = {}
-        for i in self.args:
+        for i in self.file_names:
             with open(i,'r+' , encoding='utf-8') as file:
                 text_of_words = file.read().lower()
-                for char in text_of_words:
-                    if char == ',' or char == '.' or char == '=' or char == '!' or char == '?' or char == ';' or char == ':' or char == ' - ':
-                        if char != ' - ':
-                            text_of_words = text_of_words.replace(char, '')
-                        else:
-                            text_of_words = text_of_words.replace(char, ' ' )
+                text_of_words = re.sub(' - ', ' ', text_of_words)
+                text_of_words = re.sub('[,|.|!|=|?|;|:]', '', text_of_words)
+                # for char in text_of_words:
+                #     if char == ',' or char == '.' or char == '=' or char == '!' or char == '?' or char == ';' or char == ':':
+                #         text_of_words = text_of_words.replace(char, '')
                 list_of_words = text_of_words.split()
                 all_words.update({i:list_of_words})
             #print(f'fineshed fork with file: {i}')
@@ -25,18 +24,16 @@ class WordsFinder:
         list_of_keys = name_file.keys()
         for i in list_of_keys:
             list_of_words = name_file[i]
-            for j in list_of_words:
-                if j == word.lower():
-                    with open(i, 'r', encoding='utf-8') as file:
-                        my_file = file.read()
-                        pos_of_word = my_file.find(word.lower())
-                        return {i:pos_of_word}
+            for j in range(len(list_of_words)):
+                if list_of_words[j] == word.lower():
+                    return {i:j}
         print("sorry this word can't be find")
         return {}
 
     def count(self, word):
         name_file = self.get_all_words()
         list_of_keys = name_file.keys()
+        dict_word_count = {}
         for i in list_of_keys:
             list_of_words = name_file[i]
             count_of_word = 0
@@ -44,16 +41,19 @@ class WordsFinder:
                 if j == word.lower():
                     count_of_word += 1
                     continue
+
             if count_of_word == 0:
                 continue
             else:
-                return {i: count_of_word}
+                dict_word_count.update({i: count_of_word})
+        if len(dict_word_count) == 0:
+            print("sorry this word can't be find")
+            return {}
+        else:
+            return dict_word_count
 
-        print("sorry this word can't be find")
-        return {}
 
-
-finder2 = WordsFinder('test_file.txt')
+finder2 = WordsFinder('test_file.txt', 'sample.txt', 'test.txt')
 print(finder2.get_all_words()) # Все слова
 print(finder2.find('TEXT')) # 3 слово по счёту
 print(finder2.count('teXT')) # 4 слова teXT в тексте всего
